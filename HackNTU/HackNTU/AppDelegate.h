2327ea904bf983205ddbf6cfc337244a53b34b20
Tank <tanknmm@gmail.com>
2015-08-21 14:12:40 +0800

    Initialize app project

---
+++ HackNTU/HackNTU/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HackNTU
+//
+//  Created by Tank Lin on 2015/8/21.
+//  Copyright (c) 2015年 Tank. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ HackNTU/HackNTU/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HackNTU
+//
+//  Created by Tank Lin on 2015/8/21.
+//  Copyright (c) 2015年 Tank. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
