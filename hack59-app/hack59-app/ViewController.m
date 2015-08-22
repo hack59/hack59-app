6471ce94b7a3cb0fe05268a0e72aed4d94cff38a
Tank <tanknmm@gmail.com>
2015-08-22 14:11:19 +0800

    fixed typo

--- hack59-app/hack59-app/ViewController.m
+++ hack59-app/hack59-app/ViewController.m
@@ -79,7 +79,7 @@
 }
 - (void)observeValueForKeyPath:(NSString *)keyPath ofObject:(id)object change:(NSDictionary *)change context:(void *)context
 {
-    if ([keyPath isEqual:@"myLoction"] && [object isKindOfClass:[GMSMapView class]]) {
+    if ([keyPath isEqualToString:@"myLocation"] && [object isKindOfClass:[GMSMapView class]]) {
         if (self.isFirstTimeGetMyLocation) {
             [mapView animateToCameraPosition:[GMSCameraPosition cameraWithLatitude:mapView.myLocation.coordinate.latitude longitude:mapView.myLocation.coordinate.longitude zoom:15]];
             self.isFirstTimeGetMyLocation = NO;
